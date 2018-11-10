179254eadc7eb1e3cb1a4d8e9a5c5c896595f035
perry091061 <perry091061@gmail.com>
2018-11-10 20:34:34 +0000

    Demo

---
+++ src/test/java/demo/DoSomeStuffTest.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoSomeStuffTest {
+
+    @Test
+    void doSomeStuff() {
+        DoSomeStuff doSomeStuff = new DoSomeStuff();
+        String s = doSomeStuff.doSomeStuff();
+        assertEquals("MESSAGE",s );
+    }
+}
