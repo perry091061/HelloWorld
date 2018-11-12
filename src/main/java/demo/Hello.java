ae91473c8810c1a9591ded4e823d9ea3fe2965de
perry091061 <perry091061@gmail.com>
2018-11-12 17:24:15 +0000

    Demo

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -2,7 +2,9 @@ package demo;
 
 
 public class Hello {
-
+/*
+    Check to see if teamcity triggers when an update is applied to github
+ */
 
     public static void main(String args[]) {
         DoSomeStuff doSomeStuff = new DoSomeStuff();
